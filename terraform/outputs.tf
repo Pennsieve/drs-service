14b3f1d0684fdff3fe2478bf6504fa8db0dde0f0
Michael Uftring <michael.uftring@gmail.com>
2025-04-21 13:49:49 -0400

    fix Terraform errors

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,11 @@
 output "service_lambda_arn" {
-  value = aws_lambda_function.service_lambda.arn
+  value = aws_lambda_function.drs_service_api_lambda.arn
 }
 
 output "service_lambda_invoke_arn" {
-  value = aws_lambda_function.service_lambda.invoke_arn
+  value = aws_lambda_function.drs_service_api_lambda.invoke_arn
 }
 
 output "service_lambda_function_name" {
-  value = aws_lambda_function.service_lambda.function_name
+  value = aws_lambda_function.drs_service_api_lambda.function_name
 }
