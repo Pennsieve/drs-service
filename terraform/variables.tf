a17ca42e282897781a85b1b3045ccecda0bdf968
Michael Uftring <michael.uftring@gmail.com>
2025-04-22 14:36:31 -0400

    add IAM Policy statement for RDS Proxy

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -18,6 +18,8 @@ variable "lambda_bucket" {
 
 variable "api_domain_name" {}
 
+variable "rds_proxy_user" {}
+
 locals {
   domain_name = data.terraform_remote_state.account.outputs.domain_name
   hosted_zone = data.terraform_remote_state.account.outputs.public_hosted_zone_id
@@ -27,4 +29,6 @@ locals {
     aws_region       = data.aws_region.current_region.name
     environment_name = var.environment_name
   }
+
+  rds_db_connect_arn    = "${replace(replace(data.terraform_remote_state.pennsieve_postgres.outputs.rds_proxy_endpoint_arn, ":rds:", ":rds-db:"), ":db-proxy:", ":dbuser:")}/${var.rds_proxy_user}"
 }
